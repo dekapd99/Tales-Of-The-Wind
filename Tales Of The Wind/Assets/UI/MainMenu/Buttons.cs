38370c950b68e0f5be48d419d672a846b43dc592
uzivurt0 <gue.fafa00@gmail.com>
2020-12-11 23:01:43 +0700

    Main Menu, How to Play

---
+++ Tales Of The Wind/Assets/UI/MainMenu/Buttons.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Sandbox");
+    }
+
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
